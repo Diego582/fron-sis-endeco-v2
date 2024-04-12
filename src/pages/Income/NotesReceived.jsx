207cfafd76d54d282b9d00599631863bc11ae6e4
Diego <81425000+Diego582@users.noreply.github.com>
2024-04-12 12:57:07 -0300

    menus (#4)
    Co-authored-by: Diego <penaomardiego@gmail>

---
+++ src/pages/Income/NotesReceived.jsx
@@ -0,0 +1,10 @@
+import Typography from "@mui/material/Typography";
+const NotesReceived = () => {
+  return (
+    <>
+      <Typography variant="h3">Esto es NotesReceived</Typography>
+    </>
+  );
+};
+
+export default NotesReceived;
